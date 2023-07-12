d44e0ed1c3d884429648a4064be710d867e91e22
王定龙 <1084475153@qq.com>
2023-07-12 21:57:07 +0800

    1

---
+++ WinFormsApp1/WinFormsApp1/Component1.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+
+namespace WinFormsApp1
+{
+    public partial class Component1 : Control
+    {
+        public Component1()
+        {
+            InitializeComponent();
+        }
+
+        public Component1(IContainer container)
+        {
+            container.Add(this);
+
+            InitializeComponent();
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+        }
+    }
+}
